30b5bef677b44fa4c37f93c795030c5079592307
victor-nazario <33332105+victor-nazario@users.noreply.github.com>
2018-03-26 13:03:07 -0400

    Initial upload

---
+++ src/classes/SymmetricStringAnalyzer.java
@@ -0,0 +1,48 @@
+package classes;
+
+public class SymmetricStringAnalyzer {
+	private String s; 
+	public SymmetricStringAnalyzer(String s) {
+		this.s = s; 
+	}
+	
+	/**
+	 * Determines if the string s is symmetric
+	 * @return true if it is; false, otherwise. 
+	 */
+	public boolean isValidContent() { 
+		SLLStack<Character> stack = new SLLStack<>(); 
+		for (int i = 0; i < s.length(); i++) {
+			//char c = ; 
+			if (Character.isLetter(c)); 
+			if (Character.isUpperCase(c))
+			else if () {
+				return false; 
+			}
+			
+			else {
+				char t = ""; 
+				if (t == Character.toUpperCase(c));
+				else
+					return false
+			}
+			
+			else 
+				return false; 
+		}
+		return true;  
+	}
+	
+	public String toString() { 
+		return s; 
+	}
+
+	public String parenthesizedExpression() 
+	throws StringIsNotSymmetricException 
+	{
+		// ADD MISSING CODE
+		
+		return null;  // need to change if necessary....
+	}
+
+}
